eaf66cb0fcbbe36f5fa7a92eeabcef889ccd49a5
dlaird-ovo <daniel.laird@ovo.com>
2023-03-21 08:15:20 +0000

    Add ability to use dynamic spaces

--- plugins/search-confluence-backend/src/search/ConfluenceCollatorFactory/ConfluenceCollatorFactory.ts
+++ plugins/search-confluence-backend/src/search/ConfluenceCollatorFactory/ConfluenceCollatorFactory.ts
@@ -1,214 +1,272 @@
 import { Config } from '@backstage/config';
-import { DocumentCollatorFactory, IndexableDocument } from '@backstage/plugin-search-common';
+import {
+  DocumentCollatorFactory,
+  IndexableDocument,
+} from '@backstage/plugin-search-common';
 import fetch from 'node-fetch';
 import pLimit from 'p-limit';
 import { Readable } from 'stream';
 import { Logger } from 'winston';
-import { ConfluenceDocument, ConfluenceDocumentList, IndexableAncestorRef, IndexableConfluenceDocument } from './types';
+import {
+  ConfluenceDocument,
+  ConfluenceDocumentList,
+  IndexableAncestorRef,
+  IndexableConfluenceDocument,
+  ANNOTATION_OVO_CONFLUENCE_SPACES,
+} from './types';
+import { CATALOG_FILTER_EXISTS, CatalogApi } from '@backstage/catalog-client';
+
+// Resource type
+export const RESOURCE_TYPE_CONFLUENCE_SPACES = 'confluence-spaces';
 
 type ConfluenceCollatorOptions = {
-    logger: Logger;
+  logger: Logger;
 
-    parallelismLimit: number;
+  parallelismLimit: number;
 
-    wikiUrl: string;
-    spaces: string[];
-    auth: {
-        username: string;
-        password: string;
-    };
-}
+  wikiUrl: string;
+  spaces: string[];
+  auth: {
+    username: string;
+    password: string;
+  };
+  catalogClient?: CatalogApi;
+};
 
 export interface UserEntityDocument extends IndexableDocument {
-    kind: string;
-    login: string;
-    email: string;
+  kind: string;
+  login: string;
+  email: string;
 }
 
 export class ConfluenceCollatorFactory implements DocumentCollatorFactory {
-    public readonly type: string = 'confluence';
+  public readonly type: string = 'confluence';
 
-    private logger: Logger;
+  private logger: Logger;
+  private catalogClient?: CatalogApi;
 
-    private parallelismLimit: number;
-    private wikiUrl: string;
-    private spaces: string[];
-    private auth: {username: string, password: string};
+  private parallelismLimit: number;
+  private wikiUrl: string;
+  private spaces: string[];
+  private auth: { username: string; password: string };
 
-    static fromConfig(
-        config: Config,
-        options: {
-            logger: Logger,
-            parallelismLimit?: number,
-        },
-    ) {
-        return new ConfluenceCollatorFactory({
-            logger: options.logger,
+  static fromConfig(
+    config: Config,
+    options: {
+      logger: Logger;
+      parallelismLimit?: number;
+      catalogClient?: CatalogApi;
+    },
+  ) {
+    return new ConfluenceCollatorFactory({
+      logger: options.logger,
 
-            parallelismLimit: options.parallelismLimit || 15,
+      parallelismLimit: options.parallelismLimit || 15,
 
-            wikiUrl: config.getString('confluence.wikiUrl'),
-            spaces: config.getStringArray('confluence.spaces'),
-            auth: {
-                username: config.getString('confluence.auth.username'),
-                password: config.getString('confluence.auth.password'),
-            },
-        });
+      wikiUrl: config.getString('confluence.wikiUrl'),
+      spaces: config.getStringArray('confluence.spaces'),
+      auth: {
+        username: config.getString('confluence.auth.username'),
+        password: config.getString('confluence.auth.password'),
+      },
+      catalogClient: options.catalogClient,
+    });
+  }
+
+  private constructor(options: ConfluenceCollatorOptions) {
+    this.logger = options.logger;
+
+    this.parallelismLimit = options.parallelismLimit;
+    this.wikiUrl = options.wikiUrl;
+    this.spaces = options.spaces;
+    this.auth = options.auth;
+    if (options.catalogClient) {
+      this.catalogClient = options.catalogClient;
     }
+  }
 
-    private constructor(options: ConfluenceCollatorOptions) {
-        this.logger = options.logger;
+  async getCollator() {
+    return Readable.from(this.execute());
+  }
 
-        this.parallelismLimit = options.parallelismLimit;
-        this.wikiUrl = options.wikiUrl;
-        this.spaces = options.spaces;
-        this.auth = options.auth;
-    }
+  private async *execute(): AsyncGenerator<IndexableConfluenceDocument> {
+    const spacesList = await this.getSpaces();
+    const documentsList = await this.getDocumentsFromSpaces(spacesList);
 
-    async getCollator() {
-        return Readable.from(this.execute());
-    }
-
-    private async *execute(): AsyncGenerator<IndexableConfluenceDocument> {
-        const spacesList = await this.getSpaces();
-        const documentsList = await this.getDocumentsFromSpaces(spacesList);
-
-        const limit = pLimit(this.parallelismLimit);
-        const documentsInfo = documentsList.map(document => limit(async () => {
-            try {
-                return this.getDocumentInfo(document);
-            } catch (err) {
-                this.logger.warn(`error while indexing document "${document}"`, err);
-            }
-
-            return [];
-        }));
-
-        const safePromises = documentsInfo.map(promise => promise.catch(error => {
-            this.logger.warn(error);
-
-            return [];
-        }));
-
-        const documents = (await Promise.all(safePromises)).flat();
-
-        for (const document of documents) {
-            yield document;
+    const limit = pLimit(this.parallelismLimit);
+    const documentsInfo = documentsList.map(document =>
+      limit(async () => {
+        try {
+          return this.getDocumentInfo(document);
+        } catch (err) {
+          this.logger.warn(`error while indexing document "${document}"`, err);
         }
-    }
 
-    private async getSpaces(): Promise<string[]> {
-        return this.spaces;
-    }
+        return [];
+      }),
+    );
 
-    /*
-    private async getSpaces(): Promise<string[]> {
-        const data = await this.get(
-            `${this.wikiUrl}/rest/api/space?&limit=1000&type=global&status=current`,
+    const safePromises = documentsInfo.map(promise =>
+      promise.catch(error => {
+        this.logger.warn(error);
+
+        return [];
+      }),
+    );
+
+    const documents = (await Promise.all(safePromises)).flat();
+
+    for (const document of documents) {
+      yield document;
+    }
+  }
+
+  private async getSpaces(): Promise<string[]> {
+    // If catalogClient is provided then we can try and get Resource entities of type confluence-spaces.
+    // and use those instead of fixed list.
+    if (this.catalogClient) {
+      this.logger.info(
+        `Using Resources of type ${RESOURCE_TYPE_CONFLUENCE_SPACES} to index Confluence`,
+      );
+      const confluenceSpacesKey = `metadata.annotations.${ANNOTATION_OVO_CONFLUENCE_SPACES}`;
+
+      // Create a filter so we only get the Entities we want.
+      const filter: Record<string, symbol | string> = {
+        kind: 'Resource',
+        'spec.type': RESOURCE_TYPE_CONFLUENCE_SPACES,
+        [confluenceSpacesKey]: CATALOG_FILTER_EXISTS,
+      };
+
+      const confluenceSpaces = await this.catalogClient.getEntities({
+        filter: [filter],
+      });
+      this.logger.debug(
+        `Have found ${confluenceSpaces.items.length} Resources of type confluence-spaces`,
+      );
+
+      // Parse all the entities and extract the spaces from the annotation.
+      // We support multiple resources or a single resource with CSV list.
+      const spaces: Array<string> = [];
+      confluenceSpaces.items.map(entity => {
+        const annotation =
+          entity.metadata.annotations![ANNOTATION_OVO_CONFLUENCE_SPACES]!;
+        this.logger.debug(
+          `metadata.annotations.${ANNOTATION_OVO_CONFLUENCE_SPACES}: ${annotation}`,
         );
-
-        if (!data.results) {
-            return [];
-        }
-
-        const spacesList = [];
-        for (const result of data.results) {
-            spacesList.push(result.key);
-        }
-
-        return spacesList;
+        const spaceList: string[] = annotation
+          .split(',')
+          .map((item: string) => item.trim());
+        spaces.push(...spaceList);
+      });
+      this.logger.info(`Indexing the following spaces ${spaces.toString()}`);
+      return spaces;
     }
-    */
+    return this.spaces;
+  }
 
-    private async getDocumentsFromSpaces(spaces: string[]): Promise<string[]> {
-        const documentsList = [];
+  private async getDocumentsFromSpaces(spaces: string[]): Promise<string[]> {
+    const documentsList = [];
 
-        for (const space of spaces) {
-            documentsList.push(...(await this.getDocumentsFromSpace(space)));
-        }
-
-        return documentsList;
+    for (const space of spaces) {
+      documentsList.push(...(await this.getDocumentsFromSpace(space)));
     }
 
-    private async getDocumentsFromSpace(space: string): Promise<string[]> {
-        const documentsList = [];
+    return documentsList;
+  }
 
-        this.logger.info(`exploring space ${space}`);
+  private async getDocumentsFromSpace(space: string): Promise<string[]> {
+    const documentsList = [];
 
-        let next = true;
-        let requestUrl = `${this.wikiUrl}/rest/api/content?limit=1000&status=current&spaceKey=${space}`;
-        while (next) {
-            const data = await this.get<ConfluenceDocumentList>(requestUrl);
-            if (!data.results) {
-                break;
-            }
+    this.logger.info(`exploring space ${space}`);
 
-            documentsList.push(...data.results.map(result => result._links.self));
+    let next = true;
+    let requestUrl = `${this.wikiUrl}/rest/api/content?limit=1000&status=current&spaceKey=${space}`;
+    while (next) {
+      const data = await this.get<ConfluenceDocumentList>(requestUrl);
+      if (!data.results) {
+        break;
+      }
 
-            if (data._links.next) {
-                requestUrl = `${this.wikiUrl}${data._links.next}`;
-            } else {
-                next = false;
-            }
-        }
+      documentsList.push(...data.results.map(result => result._links.self));
 
-        return documentsList;
+      if (data._links.next) {
+        requestUrl = `${this.wikiUrl}${data._links.next}`;
+      } else {
+        next = false;
+      }
     }
 
-    private async getDocumentInfo(documentUrl: string): Promise<IndexableConfluenceDocument[]> {
-        this.logger.debug(`fetching document content ${documentUrl}`);
+    return documentsList;
+  }
 
-        const data = await this.get<ConfluenceDocument>(`${documentUrl}?expand=body.storage,space,ancestors,version`);
-        if (!data.status || data.status !== 'current') {
-            return [];
-        }
+  private async getDocumentInfo(
+    documentUrl: string,
+  ): Promise<IndexableConfluenceDocument[]> {
+    this.logger.debug(`fetching document content ${documentUrl}`);
 
-        const ancestors: IndexableAncestorRef[] = [
-            {
-                title: data.space.name,
-                location: `${this.wikiUrl}${data.space._links.webui}`,
-            },
-        ];
-
-        data.ancestors.forEach(ancestor => {
-            ancestors.push({
-                title: ancestor.title,
-                location: `${this.wikiUrl}${ancestor._links.webui}`,
-            });
-        });
-
-        return [{
-            title: data.title,
-            text: this.stripHtml(data.body.storage.value),
-            location: `${this.wikiUrl}${data._links.webui}`,
-            spaceKey: data.space.key,
-            spaceName: data.space.name,
-            ancestors: ancestors,
-            lastModifiedBy: data.version.by.publicName,
-            lastModified: data.version.when,
-            lastModifiedFriendly: data.version.friendlyWhen
-        }];
+    const data = await this.get<ConfluenceDocument>(
+      `${documentUrl}?expand=body.storage,space,ancestors,version`,
+    );
+    if (!data.status || data.status !== 'current') {
+      return [];
     }
 
-    private async get<T = any>(requestUrl: string): Promise<T> {
-        const base64Auth = Buffer.from(`${this.auth.username}:${this.auth.password}`, 'utf-8').toString('base64');
-        const res = await fetch(requestUrl, {
-            method: 'get',
-            headers: {
-                Authorization: `Basic ${base64Auth}`,
-            },
-        });
+    const ancestors: IndexableAncestorRef[] = [
+      {
+        title: data.space.name,
+        location: `${this.wikiUrl}${data.space._links.webui}`,
+      },
+    ];
 
-        if (!res.ok) {
-            this.logger.warn('non-ok response from confluence', requestUrl, res.status, await res.text());
+    data.ancestors.forEach(ancestor => {
+      ancestors.push({
+        title: ancestor.title,
+        location: `${this.wikiUrl}${ancestor._links.webui}`,
+      });
+    });
 
-            throw new Error(`Request failed with ${res.status} ${res.statusText}`);
-        }
+    return [
+      {
+        title: data.title,
+        text: this.stripHtml(data.body.storage.value),
+        location: `${this.wikiUrl}${data._links.webui}`,
+        spaceKey: data.space.key,
+        spaceName: data.space.name,
+        ancestors: ancestors,
+        lastModifiedBy: data.version.by.publicName,
+        lastModified: data.version.when,
+        lastModifiedFriendly: data.version.friendlyWhen,
+      },
+    ];
+  }
 
-        return await res.json();
+  private async get<T = any>(requestUrl: string): Promise<T> {
+    const base64Auth = Buffer.from(
+      `${this.auth.username}:${this.auth.password}`,
+      'utf-8',
+    ).toString('base64');
+    const res = await fetch(requestUrl, {
+      method: 'get',
+      headers: {
+        Authorization: `Basic ${base64Auth}`,
+      },
+    });
+
+    if (!res.ok) {
+      this.logger.warn(
+        'non-ok response from confluence',
+        requestUrl,
+        res.status,
+        await res.text(),
+      );
+
+      throw new Error(`Request failed with ${res.status} ${res.statusText}`);
     }
 
-    private stripHtml(input: string): string {
-        return input.replace(/(<([^>]+)>)/gi, "");
-    }
+    return await res.json();
+  }
+
+  private stripHtml(input: string): string {
+    return input.replace(/(<([^>]+)>)/gi, '');
+  }
 }
--- plugins/search-confluence-backend/src/search/ConfluenceCollatorFactory/types.ts
+++ plugins/search-confluence-backend/src/search/ConfluenceCollatorFactory/types.ts
@@ -1,63 +1,70 @@
-import { IndexableDocument } from "@backstage/plugin-search-common";
+import { IndexableDocument } from '@backstage/plugin-search-common';
+
+/**
+ * Annotation for specifying a list of spaces to index.
+ *
+ * @public
+ */
+export const ANNOTATION_OVO_CONFLUENCE_SPACES = 'ovo.com/confluence-spaces';
 
 export type ConfluenceDocumentMetadata = {
-    title: string;
-    status: string;
+  title: string;
+  status: string;
 
-    _links: {
-        self: string;
-        webui: string;
-    };
-}
+  _links: {
+    self: string;
+    webui: string;
+  };
+};
 
 export type ConfluenceDocumentList = {
-    results: ConfluenceDocumentMetadata[];
-    _links: {
-        next: string;
-    };
-}
+  results: ConfluenceDocumentMetadata[];
+  _links: {
+    next: string;
+  };
+};
 
 export type ConfluenceDocumentAncestorRef = {
-    title: string;
+  title: string;
 
-    _links: {
-        webui: string;
-    };
+  _links: {
+    webui: string;
+  };
 };
 
 export type ConfluenceDocument = ConfluenceDocumentMetadata & {
-    body: {
-        storage: {
-            value: string;
-        };
+  body: {
+    storage: {
+      value: string;
     };
-    version: {
-        by: {
-            publicName: string,
-        },
-        when: string;
-        friendlyWhen: string;
-    }
-    space: {
-        key: string;
-        name: string;
-        _links: {
-            webui: string;
-        };
+  };
+  version: {
+    by: {
+      publicName: string;
     };
-    ancestors: ConfluenceDocumentMetadata[];
-}
+    when: string;
+    friendlyWhen: string;
+  };
+  space: {
+    key: string;
+    name: string;
+    _links: {
+      webui: string;
+    };
+  };
+  ancestors: ConfluenceDocumentMetadata[];
+};
 
 export type IndexableAncestorRef = {
-    title: string;
-    location: string;
+  title: string;
+  location: string;
 };
 
 export type IndexableConfluenceDocument = IndexableDocument & {
-    spaceKey: string;
-    spaceName: string;
-    ancestors: IndexableAncestorRef[];
-    lastModified: string;
-    lastModifiedFriendly: string;
-    lastModifiedBy: string;
+  spaceKey: string;
+  spaceName: string;
+  ancestors: IndexableAncestorRef[];
+  lastModified: string;
+  lastModifiedFriendly: string;
+  lastModifiedBy: string;
 };
